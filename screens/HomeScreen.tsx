fb1befc7f4a2a2fddf0aec75d4dd6f87d7e78587
Rokas <rookas.rudzenskas@gmail.com>
2023-01-18 15:01:09 +0200

    feat: add search bar to home screen
    - Add search bar to home screen
    - Add search icon to search bar
    - Add placeholder text to search bar
    - Add awaiting events section to home screen
    - Add airplane icon to awaiting events section
    - Add text to awaiting events section

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,11 @@
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import React from "react";
 import {StatusBar} from "expo-status-bar";
-import {AntDesign, MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
+import {AntDesign, Ionicons, MaterialCommunityIcons, MaterialIcons, Octicons} from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
@@ -21,6 +21,25 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>)
           <TouchableOpacity activeOpacity={0.7}>
             <MaterialIcons name="group-add" size={24} color="white" />
           </TouchableOpacity>
+        </View>
+      </View>
+      <Text className="text-4xl font-bold text-white mt-5 mb-3">Home</Text>
+      <View className="flex-row items-center bg-gray-900/80 rounded-md space-x-3 px-3">
+        <Octicons name="search" size={20} color="gray" />
+        <TextInput className="h-9 text-gray-300" placeholder="Search for anything in the app" />
+      </View>
+      <View className="mt-5">
+        <Text className="text-lg font-semibold text-white">Awaiting Events</Text>
+        <View>
+          <View>
+            <View>
+              <Ionicons name="ios-airplane-outline" size={24} color="white" />
+            </View>
+            <View>
+              <Text>Open Savings Account!</Text>
+              <Text>Make a Top Up</Text>
+            </View>
+          </View>
         </View>
       </View>
       <StatusBar style="auto" />
